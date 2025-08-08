99196371b07b24fc89fb7e8cb4d0ef21c5d2eb2d
Cosmic AI <support@cosmicjs.com>
2025-08-08 07:07:22 -0700

    Fix TypeScript compilation errors with proper null/undefined type handling

--- components/MovieTrailer.tsx
+++ components/MovieTrailer.tsx
@@ -24,7 +24,7 @@ export default function MovieTrailer({ trailerUrl, movieTitle, className }: Movi
   }
 
   const videoId = getYouTubeVideoId(trailerUrl)
-  const embedUrl = videoId ? `https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0` : trailerUrl
+  const embedUrl = videoId ? `https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0` : trailerUrl ?? null
 
   const openModal = () => {
     setIsModalOpen(true)
@@ -71,7 +71,7 @@ export default function MovieTrailer({ trailerUrl, movieTitle, className }: Movi
             {/* Video Embed */}
             {videoId ? (
               <iframe
-                src={embedUrl}
+                src={embedUrl ?? undefined}
                 title={`${movieTitle} Trailer`}
                 className="w-full h-full"
                 frameBorder="0"
